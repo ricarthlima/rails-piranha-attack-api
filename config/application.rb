0332e1438bddc41224277909c2b4f1de64f5f942
Ricarth Lima <rrsl@cin.ufpe.br>
2021-04-21 10:32:10 -0300

    apelacao pouca e muito

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ require "action_cable/engine"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+Mongoid.load!("config/mongoid.yml")
 
 module PiranhaAttackApi
   class Application < Rails::Application
